31821368ce65ac818f17352c21b25cfa5747671c
fangchao <fangchao@vandream.com>
2019-01-22 17:33:03 +0800

    init

---
+++ src/main/java/com/taocaicai/order/TaocaicaiOrderApplication.java
@@ -0,0 +1,14 @@
+package com.taocaicai.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaocaicaiOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaocaicaiOrderApplication.class, args);
+    }
+
+}
+
